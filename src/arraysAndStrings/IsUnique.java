ef79e0a0a1fa7ceccbf67135befa90c49e421844
Ibrahim Baz <eng.ibrahim222@gmail.com>
2019-09-17 01:38:44 +0200

    Add Problems #1

--- src/arraysAndStrings/IsUnique.java
+++ src/arraysAndStrings/IsUnique.java
@@ -8,12 +8,22 @@ public class IsUnique {
 
 
     public static void main(String[] args) {
-
+        System.out.println(isUnique("abcdefghijklmnopqrstuvwxyz0123456789.+-*/<>?|~!@#$%^&()_:;][{}"));
     }
 
-    public static boolean isUnique(String s, String word) {
+    public static boolean isUnique(String str) {
+        if (str.length() > 128)
+            return false;
 
-        
-        return false;
+        boolean[] char_set = new boolean[128];
+        for (int i = 0; i < str.length(); i++) {
+            int val = str.charAt(i);
+            if (char_set[val])
+                return false;
+
+            char_set[val] = true;
+        }
+
+        return true;
     }
 }
